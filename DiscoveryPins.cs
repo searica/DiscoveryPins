3b59a960702fb00a571f5e2ebc0417535090726f
searica <143636061+searica@users.noreply.github.com>
2024-12-15 11:05:06 -0800

    initial commit

---
+++ DiscoveryPins.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Reflection;
+using UnityEngine;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using Jotunn.Utils;
+using Jotunn.Managers;
+using DiscoveryPins.Extensions;
+using DiscoveryPins.Helpers;
+
+namespace DiscoveryPins
+{
+    [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+    [BepInDependency(Jotunn.Main.ModGuid, Jotunn.Main.Version)]
+    [NetworkCompatibility(CompatibilityLevel.VersionCheckOnly, VersionStrictness.Patch)]
+    [SynchronizationMode(AdminOnlyStrictness.IfOnServer)]
+    internal sealed class DiscoveryPins : BaseUnityPlugin
+    {
+        public const string PluginName = "DiscoveryPins";
+        internal const string Author = "Searica";
+        public const string PluginGUID = $"{Author}.Valheim.{PluginName}";
+        public const string PluginVersion = "0.1.0";
+
+
+        internal static DiscoveryPins Instance;
+        internal static ConfigFile ConfigFile;       
+
+        public void Awake()
+        {
+            Instance = this;
+            ConfigFile = Config;
+            Log.Init(Logger);
+
+            Config.Init(PluginGUID, false);
+       
+            SetUpConfigEntries();
+
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), harmonyInstanceId: PluginGUID);
+
+            Game.isModded = true;
+
+            ConfigFile.SetupWatcher();
+
+            // Re-initialization after reloading config and don't save since file was just reloaded
+            ConfigFileExtensions.OnConfigFileReloaded += () =>
+            {
+                // Do whatever is needed, currently nothing
+            };
+
+            // Re-initialize after changing config data in-game and trigger a save to disk.
+            SynchronizationManager.OnConfigurationWindowClosed += () =>
+            {
+                Config.Save();
+            };
+
+            // Re-initialize after getting updated config data and trigger a save to disk.
+            SynchronizationManager.OnConfigurationSynchronized += (obj, attr) =>
+            {
+                Config.Save();
+            };
+        }
+
+        
+
+        internal static void SetUpConfigEntries()
+        {
+    
+        }
+
+        public void OnDestroy()
+        {
+            Config.Save();
+        }
+
+    }
+
+    /// <summary>
+    ///     Log level to control output to BepInEx log
+    /// </summary>
+    internal enum LogLevel
+    {
+        Low = 0,
+        Medium = 1,
+        High = 2,
+    }
+
+    /// <summary>
+    ///     Helper class for properly logging from static contexts.
+    /// </summary>
+    internal static class Log
+    {
+        internal static ConfigEntry<LogLevel> Verbosity { get; set; }
+        internal static LogLevel VerbosityLevel => Verbosity.Value;
+        internal static bool IsVerbosityLow => Verbosity.Value >= LogLevel.Low;
+        internal static bool IsVerbosityMedium => Verbosity.Value >= LogLevel.Medium;
+        internal static bool IsVerbosityHigh => Verbosity.Value >= LogLevel.High;
+
+        private static ManualLogSource logSource;
+
+        internal static void Init(ManualLogSource logSource)
+        {
+            Log.logSource = logSource;
+        }
+
+        internal static void LogDebug(object data) => logSource.LogDebug(data);
+
+        internal static void LogError(object data) => logSource.LogError(data);
+
+        internal static void LogFatal(object data) => logSource.LogFatal(data);
+
+        internal static void LogMessage(object data) => logSource.LogMessage(data);
+
+        internal static void LogWarning(object data) => logSource.LogWarning(data);
+
+        internal static void LogInfo(object data, LogLevel level = LogLevel.Low)
+        {
+            if (Verbosity is null || VerbosityLevel >= level)
+            {
+                logSource.LogInfo(data);
+            }
+        }
+
+        internal static void LogGameObject(GameObject prefab, bool includeChildren = false)
+        {
+            LogInfo("***** " + prefab.name + " *****");
+            foreach (Component compo in prefab.GetComponents<Component>())
+            {
+                LogComponent(compo);
+            }
+
+            if (!includeChildren) { return; }
+
+            LogInfo("***** " + prefab.name + " (children) *****");
+            foreach (Transform child in prefab.transform)
+            {
+                LogInfo($" - {child.gameObject.name}");
+                foreach (Component compo in child.gameObject.GetComponents<Component>())
+                {
+                    LogComponent(compo);
+                }
+            }
+        }
+
+        internal static void LogComponent(Component compo)
+        {
+            LogInfo($"--- {compo.GetType().Name}: {compo.name} ---");
+
+            PropertyInfo[] properties = compo.GetType().GetProperties(ReflectionUtils.AllBindings);
+            foreach (var property in properties)
+            {
+                LogInfo($" - {property.Name} = {property.GetValue(compo)}");
+            }
+
+            FieldInfo[] fields = compo.GetType().GetFields(ReflectionUtils.AllBindings);
+            foreach (var field in fields)
+            {
+                LogInfo($" - {field.Name} = {field.GetValue(compo)}");
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,45 @@
+# DiscoveryPins
+Autopin mod that still makes you find stuff.
+
+## Donations/Tips
+My mods will always be free to use but if you feel like saying thanks you can tip/donate.
+
+| My Ko-fi: | [![ko-fi](https://ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/searica) |
+|-----------|---------------|
+
+## Source Code
+Source code is available on Github.
+
+| Github Repository: | <img height="18" src="https://github.githubassets.com/favicons/favicon-dark.svg"></img><a href="https://github.com/searica/MoreVanillaBuildPrefabs"> MoreVanillaBuildPrefabs</a> |
+|-----------|---------------|
+
+### Contributions
+If you would like to provide suggestions, make feature requests, or reports bugs and compatibility issues you can either open an issue on the Github repository or tag me (@searica) with a message on my discord [Searica's Mods](https://discord.gg/sFmGTBYN6n).
+
+Over time I will keep patching more prefabs and improving this mod's features but you'd really like a specific prefab to work better then let me know via one of the methods above. I'm a grad student and have a lot of personal responsibilities on top of that so I can't promise I will always respond quickly, but I do intend to maintain and improve the mod in my free time.
+
+### Credits
+This mod was inspired by QoLPins by Tekla.
+
+#### Development Credits
+- 
+
+#### Community Credits
+- Thanks to
+
+## Shameless Self Plug (Other Mods By Me)
+If you like this mod you might like some of my other ones.
+
+#### Building Mods
+- [Extra Snap Points Made Easy](https://thunderstore.io/c/valheim/p/Searica/Extra_Snap_Points_Made_Easy/)
+- [AdvancedTerrainModifiers](https://thunderstore.io/c/valheim/p/Searica/AdvancedTerrainModifiers/)
+- [BuildRestrictionTweaksSync](https://thunderstore.io/c/valheim/p/Searica/BuildRestrictionTweaksSync/)
+- [ToolTweaks](https://thunderstore.io/c/valheim/p/Searica/ToolTweaks/)
+
+#### Gameplay Mods
+- [CameraTweaks](https://thunderstore.io/c/valheim/p/Searica/CameraTweaks/)
+- [DodgeShortcut](https://thunderstore.io/c/valheim/p/Searica/DodgeShortcut/)
+- [FortifySkillsRedux](https://thunderstore.io/c/valheim/p/Searica/FortifySkillsRedux/)
+- [ProjectileTweaks](https://thunderstore.io/c/valheim/p/Searica/ProjectileTweaks/)
+- [SkilledCarryWeight](https://thunderstore.io/c/valheim/p/Searica/SkilledCarryWeight/)
+- [SafetyStatus](https://thunderstore.io/c/valheim/p/Searica/SafetyStatus/)
