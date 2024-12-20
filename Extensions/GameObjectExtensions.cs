e60a1d613be8df496b04cdf018f28b6fe85a298d
searica <143636061+searica@users.noreply.github.com>
2024-12-20 11:13:50 -0800

    Update: helper extension

---
+++ Extensions/GameObjectExtensions.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace DiscoveryPins.Extensions
+{
+    internal static class GameObjectExtensions
+    {
+        /// <summary>
+        ///     Checks if this is a prefab without an existing parent transform.
+        /// </summary>
+        /// <param name="gameObject"></param>
+        /// <returns></returns>
+        internal static bool IsTopLevelPrefab(this GameObject gameObject)
+        {
+            return !gameObject.transform.parent;
+        }
+    }
+}
