a2aba73146acce5f692a467d7012770da5857b6d
Searica <143636061+searica@users.noreply.github.com>
2025-02-09 01:30:30 -0800

    Future pin types

---
+++ Patches/TarPins.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+
+namespace DiscoveryPins.Patches
+{
+    [HarmonyPatch]
+    internal static class TarPins
+    {
+        public static string name = "Tar";
+
+        // maybe awake on liquid volume and check if it is tar?
+    }
+}
